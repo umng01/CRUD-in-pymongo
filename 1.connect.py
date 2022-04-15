c1522d3078f1897ba932906a56ab29cd15f7b920
Umang Sharma <umng01@gmail.com>
2022-04-15 11:39:39 +0530

    editing

--- 1.connect.py
+++ 1.connect.py
@@ -6,7 +6,7 @@ import pymongo
 if __name__ == "__main__":
 
     client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client['TestData']
-    collection = db['KeyValuePair']
+    db = client['umang']
+    collection = db['mySampleCollection']
     
 
--- 2.insert.py
+++ 2.insert.py
@@ -82,15 +82,3 @@ if __name__ == "__main__":
     collection.insert_many(insertThese)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
